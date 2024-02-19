ef255ba14c9d9b3a5bebe62485b0d9784aff5268
JOHN AARON GARCIA <jmg5819@students.uc-bcf.edu.ph>
2024-02-19 08:04:55 +0800

    test

--- Engine/Models/Location.cs
+++ Engine/Models/Location.cs
@@ -8,7 +8,7 @@ namespace Engine.Models
 {
     public class Location
     {
-        public int XCoordinate { get; set; }
+        public int XCoordinate { get; set; } }
         public int YCoordinate { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
