67f1e5605dc2d53ae8b2303d33493cd2d66f5392
JOHN AARON GARCIA <jmg5819@students.uc-bcf.edu.ph>
2024-03-03 22:34:36 +0800

    Added location descriptions

--- Engine/Factories/WorldFactory.cs
+++ Engine/Factories/WorldFactory.cs
@@ -13,15 +13,15 @@ namespace Engine.Factories
         {
             World newWorld = new World();
 
-            newWorld.AddLocation(0,0, "Home", "Placeholder", "pack://application:,,,/Engine;component/Images/Locations/home.png");
-            newWorld.AddLocation(0, 1, "Enchanted Forest", "Placeholder", "pack://application:,,,/Engine;component/Images/Locations/enchantedforest.png");
-            newWorld.AddLocation(1, 1, "Caverns of Shadows", "Placeholder", "pack://application:,,,/Engine;component/Images/Locations/cavernsofshadows.png");
-            newWorld.AddLocation(-1, 1, "Volcanic Forge", "Placeholder", "pack://application:,,,/Engine;component/Images/Locations/volcanicforge.png");
-            newWorld.AddLocation(0, 2, "City of Ancients", "Placeholder", "pack://application:,,,/Engine;component/Images/Locations/cityofancients.png");
-            newWorld.AddLocation(1, 2, "Skyward Summit", "Placeholder", "pack://application:,,,/Engine;component/Images/Locations/skywardsummit.png");
-            newWorld.AddLocation(-1, 2, "Dragon's Peak", "Placeholder", "pack://application:,,,/Engine;component/Images/Locations/dragonspeak.png");
-            newWorld.AddLocation(-1, 0, "Haunted Graveyard", "Placeholder", "pack://application:,,,/Engine;component/Images/Locations/hauntedgraveyard.png");
-            newWorld.AddLocation(1, 0, "Crystal Lake Shore", "Placeholder", "pack://application:,,,/Engine;component/Images/Locations/crystallakeshore.png");
+            newWorld.AddLocation(0,0, "Home", "A peaceful village where your journey begins", "pack://application:,,,/Engine;component/Images/Locations/home.png");
+            newWorld.AddLocation(0, 1, "Enchanted Forest", "Dense woods filled with magical creatures", "pack://application:,,,/Engine;component/Images/Locations/enchantedforest.png");
+            newWorld.AddLocation(1, 1, "Caverns of Shadows", "A dark labyrinthine cave system", "pack://application:,,,/Engine;component/Images/Locations/cavernsofshadows.png");
+            newWorld.AddLocation(-1, 1, "Volcanic Forge", "Fiery landscape with rivers of lava", "pack://application:,,,/Engine;component/Images/Locations/volcanicforge.png");
+            newWorld.AddLocation(0, 2, "City of Ancients", "A once-great city now in ruins", "pack://application:,,,/Engine;component/Images/Locations/cityofancients.png");
+            newWorld.AddLocation(1, 2, "Skyward Summit", "A floating island in the sky", "pack://application:,,,/Engine;component/Images/Locations/skywardsummit.png");
+            newWorld.AddLocation(-1, 2, "Dragon's Peak", "A towering mountain where dragons reside", "pack://application:,,,/Engine;component/Images/Locations/dragonspeak.png");
+            newWorld.AddLocation(-1, 0, "Haunted Graveyard", "Eerie graveyard with restless spirits", "pack://application:,,,/Engine;component/Images/Locations/hauntedgraveyard.png");
+            newWorld.AddLocation(1, 0, "Crystal Lake Shore", "A serene lake surrounded by crystalline formations", "pack://application:,,,/Engine;component/Images/Locations/crystallakeshore.png");
 
             return newWorld;
         }
